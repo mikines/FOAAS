be1df43c9e5d39b6a5d2c0a5a8266de03ba24be2
kkrotW <kankanrangta@gmail.com>
2017-10-25 21:09:28 -0400

    Update views.py

--- SHARED/FOASS/fuckoff/views.py
+++ SHARED/FOASS/fuckoff/views.py
@@ -11,4 +11,4 @@ import requests
 def index(request):
 #	return HttpResponse("Hello, world. You're at the polls index.")
 	url = "https://www.foaas.com/asshole/:from me"
-	return requests.post(url)
+	return requests.get(url)
