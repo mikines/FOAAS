be39c081800aeef1e5f1784477d344d8f930694d
kkrotW <kankanrangta@gmail.com>
2017-10-25 21:19:31 -0400

    Update views.py

--- SHARED/FOASS/fuckoff/views.py
+++ SHARED/FOASS/fuckoff/views.py
@@ -6,9 +6,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-import requests
+from requests import *
 
 def index(request):
 #	return HttpResponse("Hello, world. You're at the polls index.")
 	url = "https://www.foaas.com/asshole/:from me"
-	return requests.GET.get(url)
+	return requests.get(url)
