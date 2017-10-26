6b0aeb701888ec0983aae67ece14237659b16ef3
kkrotW <kankanrangta@gmail.com>
2017-10-25 22:14:03 -0400

    Update views.py

--- SHARED/FOASS/fuckoff/views.py
+++ SHARED/FOASS/fuckoff/views.py
@@ -19,5 +19,5 @@ def index(request):
 	#t = Template(<form method="POST"><input type="text" name="search"><input type="submit"></form>)
 	#html = t.render(Context())
 	#t = Template('<html><form method="POST"><input type="text" name="search"><input type="submit" value="submit"></form></html>')
-	html = '<html>{% csrf_token %}<form method="POST"><input type="text" name="search"><input type="submit" value="submit"></form></html>'
+	html = '<html><form method="POST">{% csrf_token %}<input type="text" name="search"><input type="submit" value="submit"></form></html>'
 	return HttpResponse(html)
