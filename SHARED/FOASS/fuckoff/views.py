0c46dfd84801a81e130b25043c5cf619d1dd3ef2
mikines <mikseni@bu.edu>
2017-10-25 21:38:53 -0400

    api call

--- SHARED/FOASS/fuckoff/views.py
+++ SHARED/FOASS/fuckoff/views.py
@@ -10,5 +10,8 @@ import requests
 
 def index(request):
 #	return HttpResponse("Hello, world. You're at the polls index.")
-	r = requests.get("https://www.foaas.com/asshole/:from me")
-	return HttpResponse(r.text)
+	if request.method = 'POST':
+		search = request.POST['search']
+		r = requests.get("https://www.foaas.com/"+search)
+		return HttpResponse(r.text)
+	return render(request, 'hello.html')
