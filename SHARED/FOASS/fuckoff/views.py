9866187b7d1a913458c5eaaf6e848b6f88ed3800
mikines <mikseni@bu.edu>
2017-10-25 21:06:47 -0400

    api call

--- SHARED/FOASS/fuckoff/views.py
+++ SHARED/FOASS/fuckoff/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+import requests
 
 def index(request):
 #	return HttpResponse("Hello, world. You're at the polls index.")
