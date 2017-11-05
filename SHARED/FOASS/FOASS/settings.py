a0ec797e6011eec678e733755bb3ec0fe3e9914c
mikines <mikseni@bu.edu>
2017-11-05 15:30:10 -0500

    secrets

--- SHARED/FOASS/FOASS/settings.py
+++ SHARED/FOASS/FOASS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sln+2_s3r8!m&4nd9ia4%9(jkckh$!c-9n3p6)mrrxk%b^5^-r'
+SECRET_KEY = secrets.django_secrets()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
