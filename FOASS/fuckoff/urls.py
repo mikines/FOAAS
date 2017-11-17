35000c0108fb59a385c278a05664273a99b99b2d
mikines <mikseni@bu.edu>
2017-11-17 02:25:52 +0000

    login,profile pages and getting text of foaas api

--- FOASS/fuckoff/urls.py
+++ FOASS/fuckoff/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.login),
+	url(r'^style.css',TemplateView.as_view(template_name="style.css")),
 ]
--- FOASS/twilio/test.py
+++ FOASS/twilio/test.py
@@ -4,9 +4,8 @@ import twi,twi_nums
 account_sid = twi.sid()
 auth_token = twi.tok()
 
+client = Client(account_sid,auth_token)
 def send():
-  client = Client(account_sid,auth_token)
-
   message = client.messages.create(
     to=twi_nums.get_number(),
     from_=twi_nums.from_num(),
@@ -14,4 +13,11 @@ def send():
 
 #print message.sid
 
-send()
+def add_caller(number):
+  validation_request = client.validation_requests.create(number,friendly_name="test")
+  print validation_request.validation_code
+
+add_caller(twi_nums.get_test())
+#send()
+
+
